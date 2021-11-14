3804be6cdbf05761a3052e076624932b7cf0cc2e
Sagar Rajkumar Gupta <90018665+213sagar@users.noreply.github.com>
2021-11-14 14:37:10 +0530

    product of two number that is user enterd format

---
+++ product_of_two_numbers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float x,y,a;
+    cout<<"Enter two numbers : ";
+
+    cin>>x>>y;
+    a=x*y;
+    cout<< "the product of two number is : "<<a;
+
+
+
+    return 0;
+}
